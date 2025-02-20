31724403aa0074ba164c689261afe6166d04b0e1
SergeySkorikov <ap0llyon@yandex.ru>
2025-02-20 16:33:08 +0300

    Выполнено задание спринта 6

---
+++ MovieQuiz/Services/MoviesLoader.swift
@@ -0,0 +1,39 @@
+//
+//  MoviesLoader.swift
+//  MovieQuiz
+//
+//  Created by Сергей Скориков on 19.02.2025.
+//
+
+import Foundation
+
+protocol MoviesLoading {
+    func loadMovies(handler: @escaping (Result<MostPopularMovies, Error>) -> Void)
+}
+
+struct MoviesLoader: MoviesLoading {
+    private let networkClient = NetworkClient()
+    
+    private var mostPopularMoviesURL: URL {
+        guard let url = URL(string: "https://tv-api.com/en/API/Top250Movies/k_zcuw1ytf") else {
+            preconditionFailure("Unable to construct mostPopularMoviesUrl")
+        }
+        return url
+    }
+    
+    func loadMovies(handler: @escaping (Result<MostPopularMovies, Error>) -> Void) {
+        networkClient.fetch(url: mostPopularMoviesURL) { result in
+            switch result {
+            case .success(let data):
+                do {
+                    let movies = try JSONDecoder().decode(MostPopularMovies.self, from: data)
+                    handler(.success(movies))
+                } catch {
+                    handler(.failure(error))
+                }
+            case .failure(let error):
+                handler(.failure(error))
+            }
+        }
+    }
+}
